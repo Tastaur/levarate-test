a0728cc851a4803a0a825af0a98f1f1a5c07c97c
marksobolev <mark.sobolev@gotech.io>
2024-05-06 19:19:58 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-import {useCountryData} from "./hooks/useCountryData";
-import {usePeopleData} from "./hooks/usePeopleData";
+import {usePeopleListData} from "./hooks/usePeopleListData";
 
 const App: React.FunctionComponent = () => {
-    const {countries} = useCountryData()
-    const {peoples, setSearch, search, loading} = usePeopleData()
+    const {
+        peopleList,
+        loading,
+        setSearch,
+        search,
+        peopleCount
+    } = usePeopleListData()
+
     return (
         <div className="pageWrapper">
             <p>Search Component</p>
             <input value={search} onChange={e => setSearch(e.target.value)}/>
             <p>List Component</p>
             <div className="listWrapper">
-                {loading ? <div>Loading...</div> : peoples?.searchResults.map(i => <div key={i.id}>
-                    <p>{i.first_name} {i.last_name} ({countries[i.country]?.name})</p>
-                    <img className="flag" height={50} src={countries[i.country]?.flag} alt="Flag alt"/>
+                {loading ? <div>Loading...</div> : peopleList.map(i => <div key={i.id}>
+                    <p>{i.first_name} {i.last_name} ({i.country}, {i.age})</p>
+                    <img className="flag" height={50} src={i.flag} alt="Flag alt"/>
                 </div>)}
             </div>
-            <p>Found results: {peoples?.searchResultCount}</p>
-            <p>Total results: {peoples?.totalResultCounter}</p>
+            <p>Found results: {peopleList.length}</p>
+            <p>Total results: {peopleCount}</p>
         </div>
     );
 };
---
+++ src/hooks/usePeopleListData.ts
@@ -0,0 +1,28 @@
+import {useCountryData} from "./useCountryData";
+import {usePeopleData} from "./usePeopleData";
+import {useMemo, useState} from "react";
+
+export const usePeopleListData = () => {
+    const [search, setSearch] = useState('')
+    const {countries} = useCountryData()
+    const {peoples, loading} = usePeopleData()
+
+    const peopleList = useMemo(() => {
+        const regex = new RegExp(search)
+        return peoples?.searchResults.filter(i => regex.test(i.first_name) || regex.test(i.last_name)).map(i => ({
+            ...i,
+            countryName: countries[i.country].name,
+            age: new Date().getFullYear() - new Date(i.date_of_birth).getFullYear(),
+            flag: countries[i.country].flag
+        })) ?? []
+    }, [countries, peoples, search])
+
+    return {
+        search,
+        setSearch,
+        countries,
+        loading,
+        peopleList,
+        peopleCount: peoples?.totalResultCounter
+    }
+}
